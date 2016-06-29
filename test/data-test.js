e88937c9ff7bb9f8b12b2f8bc849de7aa7c914ba
jojodee <joan@projectcodex.co>
2016-06-29 17:22:33 +0200

    updating tests

--- test/data-test.js
+++ test/data-test.js
@@ -1,58 +1,68 @@
 var assert = require("assert");
 var readFile = require("../readFile");
 
+
+var saleObject = readFile.weekSales('./CSV_files/week1.csv');
+
+
 describe('weekly sales data', function() {
 
-  it(' should return most popular product  for  week 1', function() {
+  it(' should return length of sales array for week1', function() {
 
-    var week1MostPopularProduct = {
-      'Milk 1l': 39,
-      Imasi: 30,
-      Bread: 45,
-      'Chakalaka Can': 23,
-      'Gold Dish Vegetable Curry Can': 17,
-      'Fanta 500ml': 33,
-      'Coke 500ml': 54,
-      'Cream Soda 500ml': 22,
-      'Iwisa Pap 5kg': 17,
-      'Top Class Soy Mince': 22,
-      'Shampoo 1 litre': 3,
-      'Soap Bar': 12,
-      'Bananas - loose': 47,
-      'Apples - loose': 36,
-      'Mixed Sweets 5s': 49
-    };
+    var result = readFile.weekSales('./CSV_files/week1.csv').length;
 
-    assert.deepEqual(week1MostPopularProduct, readFile.weekSales('./CSV_files/week1.csv').length);
+    assert.deepEqual(result, 105);
 
 
   })
-  it('should return most popular product  for  week 2', function() {
+  it(' should return length of sales array for week2', function() {
 
-    var week2MostPopularProduct = 'milk';
+    var result = readFile.weekSales('./CSV_files/week2.csv').length;
 
-    assert.deepEqual(week2GroupedSales, readFile.groupedSales('./CSV_files/week2.csv').length);
+    assert.deepEqual(result, 117);
+
 
 
   })
-  it('should return most popular product for  week 3', function() {
+  it(' should return length of sales array for week3', function() {
 
-    var week3MostPopularProduct = 'milk';
+    var result = readFile.weekSales('./CSV_files/week3.csv').length;
 
-    assert.deepEqual(week3GroupedSales, readFile.groupedSales('./CSV_files/week3.csv').length);
-
-
-  })
-  it('should return most popular product  for  week 4', function() {
-
-    var week4MostPopularProduct = 'milk';
-
-    assert.deepEqual(week4GroupedSales, readFile.groupedSales('./CSV_files/week4.csv').length);
+    assert.deepEqual(result, 104);
 
 
   })
+  it(' should return length of sales array for week4', function() {
+
+    var result = readFile.weekSales('./CSV_files/week4.csv').length;
+
+    assert.deepEqual(result, 119);
+  })
+
+})
 
 
+describe('sales data', function(){
 
+  it('should return most popular product for week1', function(){
+
+    var saleObject = [{
+      Product: 'Milo',
+      Quantity: 7,
+    },{
+      Product: 'Peanuts',
+      Quantity: 15,
+    }, {
+      Product: 'Milo',
+      Quantity: 15,
+    }];
+
+    var result = readFile.processor(saleObject)
+
+    assert.deepEqual(result, {
+      Milo : 22,
+      Peanuts : 15
+    })
+  })
 
 })
